183f4960bab03fb9d0065f87adda1930b67987b4
giorgirogava <giorgirogava01ai@gmail.com>
2021-11-23 17:10:05 +0400

    Initial Commit

---
+++ homework_25/homework_25App.swift
@@ -0,0 +1,17 @@
+//
+//  homework_25App.swift
+//  homework_25
+//
+//  Created by IMAC on 23.11.21.
+//
+
+import SwiftUI
+
+@main
+struct homework_25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
